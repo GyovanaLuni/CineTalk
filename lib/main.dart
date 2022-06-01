3096a1bfa6a0adbc3dd0585ffb3a421f94b15b15
GyovanaLuni <gyoluni235@gmail.com>
2022-06-01 13:54:25 -0300

    conexão com banco

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'widg.dart';
 
-main (){
+main() async {
   // ignore: avoid_unnecessary_containers
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(AppW());
 }
-
-
