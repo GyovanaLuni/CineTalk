27accb5be615a9e63a200981efe06e813a3dae35
Duarte <matheus934qwer@gmail.com>
2022-06-16 17:15:28 -0300

    guarda3

--- firebase-init.js
+++ firebase-init.js
@@ -6,5 +6,5 @@ const firebaseConfig = {
     storageBucket: "cinetalk1-1a731.appspot.com",
     messagingSenderId: "777077118164",
     appId: "1:777077118164:web:e4961fdc029f44969d1741"
-  };
-  firebase.initializeApp(firebaseConfig);
+};
+firebase.initializeApp(firebaseConfig);
